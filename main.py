33522786d0fb2fed20d19a0e94362372775020ef
leo <2223128221@qq.com>
2024-07-25 15:07:41 +0800

    [feat]Implementing ChatRCA.

---
+++ main.py
@@ -0,0 +1,14 @@
+from Agents import *
+from Tooluse import *
+from CSV2md import remove_suffix
+
+
+
+if __name__ == '__main__':
+    fault_time = read_txt_as_json()["inject_time"]
+    fault_pod_suffix = read_txt_as_json()["inject_pod"]
+    fault_pod = remove_suffix(remove_suffix(fault_pod_suffix))
+    chat_result = User_proxy.initiate_chat(
+        Group_chat_manager,
+        message="The current cloud system experienced a failure at " + fault_time + ". The current " + fault_pod + " service is affected. Please analyze the root cause.",
+    )
