7dd29bd1b6d9880dd1cd5d7b85056ded07ecbb44
leo <2223128221@qq.com>
2024-07-25 15:42:28 +0800

    [feat]GPT-3.5 Turbo/4o Prompted

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import re
 from Agents import *
 from Tooluse import *
 from CSV2md import remove_suffix
-
+# 清除倒数第一个“-”及以后的字符
 
 
 if __name__ == '__main__':
     fault_time = read_txt_as_json()["inject_time"]
     fault_pod_suffix = read_txt_as_json()["inject_pod"]
     fault_pod = remove_suffix(remove_suffix(fault_pod_suffix))
-    chat_result = User_proxy.initiate_chat(
-        Group_chat_manager,
-        message="The current cloud system experienced a failure at " + fault_time + ". The current " + fault_pod + " service is affected. Please analyze the root cause.",
+    prompt = alldata() + '\n' +"""You are an operations engineer who is proficient in root cause analysis of cloud events.Existing fault types include return, exception, network delay, cpu content.
+    The current cloud system experienced a failure at """ + fault_time + """. The current """ + fault_pod  + """service is affected. Please analyze the root cause.You are responsible for predicting root cause base on existing data. Do not give results beyond the four failure types in the current system.
+    """
+    User_proxy.initiate_chat(
+        Assistant,
+        message=prompt,
     )
